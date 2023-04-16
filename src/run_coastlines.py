81f633a6372f890488b078935b32aa1d1a691612
Jesse Anderson <jesse.anderson@gmail.com>
2023-04-16 11:06:59 -0700

    added awei

--- src/run_coastlines.py
+++ src/run_coastlines.py
@@ -5,9 +5,11 @@ from dask_gateway import GatewayCluster
 import geopandas as gpd
 from pandas import DatetimeIndex, Timestamp
 import rioxarray
+from retry import retry
 from xarray import DataArray, Dataset
 
 from dep_tools.Processor import Processor
+from dep_tools.utils import scale_and_offset
 
 
 def mndwi(xr: DataArray) -> DataArray:
@@ -25,6 +27,72 @@ def ndwi(xr: DataArray) -> DataArray:
     nir = xr.sel(band="nir08")
     ndwi = (green - nir) / (green + nir)
     return ndwi.rename("ndwi")
+
+
+def awei(xr: DataArray) -> DataArray:
+    green = xr.sel(band="green")
+    swir1 = xr.sel(band="swir16")
+    swir2 = xr.sel(band="swir22")
+    nir = xr.sel(band="nir08")
+
+    awei = 4 * (green - swir2) - (0.25 * nir + 2.75 * swir1)
+    return awei.rename("awei")
+
+
+def normalized_ratio(band1: DataArray, band2: DataArray) -> DataArray:
+    return (band1 - band2) / (band1 + band2)
+
+
+def wofs(tm_da: DataArray) -> DataArray:
+    # lX indicates a left path from node X
+    # rX indicates a right
+    # dX is just the logic for _that_ node
+    tm = tm_da.to_dataset("band")
+    tm["ndi52"] = normalized_ratio(tm.swir16, tm.green)
+    tm["ndi43"] = normalized_ratio(tm.nir08, tm.red)
+    tm["ndi72"] = normalized_ratio(tm.swir22, tm.green)
+
+    d1 = tm.ndi52 <= -0.01
+    l2 = d1 & (tm.blue <= 2083.5)
+    d3 = tm.swir22 <= 323.5
+
+    l3 = l2 & d3
+    w1 = l3 & (tm.ndi43 <= 0.61)
+
+    r3 = l2 & ~d3
+    d5 = tm.blue <= 1400.5
+    d6 = tm.ndi72 <= -0.23
+    d7 = tm.ndi43 <= 0.22
+    w2 = r3 & d5 & d6 & d7
+
+    w3 = r3 & d5 & d6 & ~d7 & (tm.blue <= 473.0)
+
+    w4 = r3 & d5 & ~d6 & (tm.blue <= 379.0)
+    w7 = r3 & ~d5 & (tm.ndi43 <= -0.01)
+
+    d11 = tm.ndi52 <= 0.23
+    l13 = ~d1 & d11 & (tm.blue <= 334.5) & (tm.ndi43 <= 0.54)
+    d14 = tm.ndi52 <= -0.12
+
+    w5 = l13 & d14
+    r14 = l13 & ~d14
+    d15 = tm.red <= 364.5
+
+    w6 = r14 & d15 & (tm.blue <= 129.5)
+    w8 = r14 & ~d15 & (tm.blue <= 300.5)
+
+    w10 = (
+        ~d1
+        & ~d11
+        & (tm.ndi52 <= 0.32)
+        & (tm.blue <= 249.5)
+        & (tm.ndi43 <= 0.45)
+        & (tm.red <= 364.5)
+        & (tm.blue <= 129.5)
+    )
+
+    water = w1 | w2 | w3 | w4 | w5 | w6 | w7 | w8 | w10
+    return water.where(tm.red.notnull(), float("nan"))
 
 
 def filter_by_cutoffs(
@@ -52,6 +120,7 @@ def filter_by_cutoffs(
     return ds.where(tide_bool_highres)
 
 
+@retry(tries=20, delay=10)
 def load_tides(
     path,
     row,
@@ -108,6 +177,13 @@ def coastlines_by_year(xr: DataArray, area) -> Dataset:
     xr = xr.drop_duplicates(...)
     working_ds = mndwi(xr).to_dataset()
     working_ds["ndwi"] = ndwi(xr)
+    working_ds["awei"] = awei(xr)
+
+    # recaling for the wofs algorithm
+    l1_scale = 0.0001
+    l1_rescale = 1.0 / l1_scale
+    xr = scale_and_offset(xr, scale=[l1_rescale])
+    working_ds["wofs"] = wofs(xr)
 
     tides_lowres = load_tides(area["PATH"].values[0], area["ROW"].values[0])
     # Filter out times that are not in the tidal data. Basically because I have
@@ -116,6 +192,12 @@ def coastlines_by_year(xr: DataArray, area) -> Dataset:
     working_ds = working_ds.sel(
         time=working_ds.time[working_ds.time.isin(tides_lowres.time)]
     )
+
+    # Now filter out tide times that are not in the working_ds
+    tides_lowres = tides_lowres.sel(
+        time=tides_lowres.time[tides_lowres.time.isin(working_ds.time)]
+    )
+
     # The deafrica-coastlines code uses rio.reproject_match, but it is not dask
     # enabled. However, we shouldn't need the reprojection, so we can use
     # (the dask enabled) DataArray.interp instead. Note that the default resampler
@@ -137,12 +219,8 @@ def coastlines_by_year(xr: DataArray, area) -> Dataset:
     if len(working_ds.time) == 0:
         return None
 
-    # This taken from tidal_composites (I would use it directly but it
-    # sets different nodata values which our writer can't handle, and adds a year dimension (likewise) median_ds = working_ds.median(dim="time", keep_attrs=True) median_ds["count"] = working_ds.mndwi.count(dim="time", keep_attrs=True).astype(
-    #        "int16"
-    #    )
-    #    median_ds["stdev"] = working_ds.mndwi.std(dim="time", keep_attrs=True)
     median_ds = working_ds.resample(time="1Y").median(keep_attrs=True)
+    median_ds["wofs"] = working_ds.wofs.resample(time="1Y").mean(keep_attrs=True)
     median_ds["count"] = (
         working_ds.mndwi.resample(time="1Y").count(keep_attrs=True).astype("int16")
     )
@@ -151,8 +229,7 @@ def coastlines_by_year(xr: DataArray, area) -> Dataset:
         time=[f"{t.year}" for t in DatetimeIndex(median_ds.time)]
     )
 
-    # return median_ds.ndwi.to_dataset("time")
-    return median_ds
+    return median_ds.squeeze()
 
 
 def run_processor(
@@ -184,7 +261,9 @@ if __name__ == "__main__":
         aoi_by_tile=aoi_by_tile,
         convert_output_to_int16=True,
         send_area_to_scene_processor=True,
+        # year="2015",
         split_output_by_year=True,
         split_output_by_variable=False,
         overwrite=False,
+        output_value_multiplier=1000,
     )
