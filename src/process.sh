b7a245b76af4af871d0e274184363d68d169d37d
Jesse Anderson <jesse.anderson@gmail.com>
2023-04-17 09:47:13 -0700

    renamed raster calculation files

--- src/process.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /var/share/tide_models
-tar xvf /tmp/src/data/TPXO9_atlas_v5.tar.xz -C /var/share/tide_models
-rm /tmp/src/data/TPXO9_atlas_v5.tar.xz
-cp /tmp/src/data/coastline_split_by_pathrow.gpkg ./
-python run_coastlines.py
