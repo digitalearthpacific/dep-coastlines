3c2715a769dfadf3215673e602be1f07a46fbcbc
Jesse Anderson <jesse.anderson@gmail.com>
2023-09-19 17:14:53 -0700

    fixed up vector merging

--- src/merge_vectors.sh
+++ src/merge_vectors.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 version=$1
-az_src="coastlines/$version/lines"
+clean_src=$2
+prefix=$1/$2
+az_src="coastlines/$prefix-lines"
 dst=data/$version
-output=data/$version/coastlines_$version.gpkg
-output_geojson=data/$version/coastlines_$version.geojson
-tile_folder=data/tiles/$version
+output=data/$version/$clean_src-$version.gpkg
+output_geojson=data/$version/$clean_src-$version.geojson
+tile_folder=data/tiles/$clean_src-$version
 
 mkdir -p $dst
 az storage fs directory download -f output -s $az_src -d $dst --recursive 
-ogrmerge.py -o tmp.gpkg $dst/lines/*.gpkg -field_strategy Union -single -nln coastlines -overwrite_ds
-# Fixes feature misordering across files
+ogrmerge.py -o tmp.gpkg $dst/$clean_src-lines/*.gpkg -field_strategy Union -single -nln coastlines -overwrite_ds
+## Fixes feature misordering across files
 ogr2ogr $output -dialect sqlite tmp.gpkg -nln coastlines -sql "select year as year, st_union(geom) as geom from coastlines group by year"
 rm tmp.gpkg
 
@@ -20,5 +22,5 @@ python src/fix_lines_across_antimeridian.py $output $output_geojson
 mkdir -p $tile_folder
 tippecanoe -e $tile_folder/coastlines -z 12 -l coastlines $output_geojson --force
 
-azcopy copy $tile_folder/coastlines https://deppcpublicstorage.blob.core.windows.net/output/tiles/$version/?$AZURE_STORAGE_SAS_TOKEN --from-to=LocalBlob --blob-type BlockBlob --content-type application/vnd.mapbox-vector-tile --content-encoding gzip --recursive
+azcopy copy $tile_folder/coastlines https://deppcpublicstorage.blob.core.windows.net/output/tiles/$prefix/?$AZURE_STORAGE_SAS_TOKEN --from-to=LocalBlob --blob-type BlockBlob --content-type application/vnd.mapbox-vector-tile --content-encoding gzip --recursive
 
--- src/tools/run_on_pc.sh
+++ src/tools/run_on_pc.sh
@@ -3,4 +3,5 @@
 pip install "git+https://github.com/digitalearthpacific/dep-tools.git@coastline-fixes"
 pip install "git+https://github.com/jessjaco/azure-logger.git@0376f92"
 pip install --force-reinstall --no-deps "git+https://github.com/jessjaco/odc-stac.git@exception-fix"
+# pip install "git+https://github.com/digitalearthafrica/deafrica-coastlines@75dfe83"
 python "$@"
