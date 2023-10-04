a5a3212902d2566451476a74753d3f01adad61d6
Jesse Anderson <jesse.anderson@gmail.com>
2023-10-03 18:09:01 -0700

    updated merge code again

--- src/merge_vectors.sh
+++ src/merge_vectors.sh
@@ -12,19 +12,19 @@ output_geojson=data/$version/coastlines_$version.geojson
 output_4326=data/$version/coastlines_${version}_4326.gpkg
 tile_folder=data/tiles/coastlines-$version
 
-#mkdir -p $dst
-#az storage fs directory download -f output -s $az_src -d $dst --recursive 
-#ogrmerge.py -o tmp.gpkg -t_srs $dst/$clean_src-lines/*.gpkg -field_strategy Union -single -nln coastlines -overwrite_ds -t_srs EPSG:$gpkg_epsg
+mkdir -p $dst
+az storage fs directory download -f output -s $az_src -d $dst --recursive 
+ogrmerge.py -o tmp.gpkg -t_srs $dst/$clean_src-lines/*.gpkg -field_strategy Union -single -nln coastlines -overwrite_ds -t_srs EPSG:$gpkg_epsg
 ## Fixes feature misordering across files
-#ogr2ogr $output -dialect sqlite tmp.gpkg -nln coastlines -sql "select year as year, st_union(geom) as geom from coastlines group by year"
-#rm tmp.gpkg
-#
-#python src/fix_lines_across_antimeridian.py $output $output_4326
-#
-#ogr2ogr $output_geojson $output_4326
-#
+ogr2ogr $output -dialect sqlite tmp.gpkg -nln coastlines -sql "select year as year, st_union(geom) as geom from coastlines group by year"
+rm tmp.gpkg
+
+python src/fix_lines_across_antimeridian.py $output $output_4326
+
+ogr2ogr $output_geojson $output_4326
+
 azcopy copy $output https://deppcpublicstorage.blob.core.windows.net/output/coastlines/$version/?$AZURE_STORAGE_SAS_TOKEN --from-to=LocalBlob --blob-type BlockBlob 
-#
+
 azcopy copy $output_geojson https://deppcpublicstorage.blob.core.windows.net/output/coastlines/$version/?$AZURE_STORAGE_SAS_TOKEN --from-to=LocalBlob --blob-type BlockBlob 
 #
 mkdir -p $tile_folder
