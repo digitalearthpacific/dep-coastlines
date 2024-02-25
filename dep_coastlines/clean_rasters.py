7ca9e10d8fd6ca1cd655dcd2041271c9f9e21e44
Jesse Anderson <jesse.anderson@gmail.com>
2024-02-25 16:27:08 -0700

    ndwi 1,3,5

--- dep_coastlines/clean_rasters.py
+++ dep_coastlines/clean_rasters.py
@@ -229,7 +229,7 @@ def run(
     )
 
     loader = MultiyearMosaicLoader(
-        start_year=start_year, end_year=end_year, years_per_composite=[3, 5]
+        start_year=start_year, end_year=end_year, years_per_composite=[1, 3, 5]
     )
     processor = Cleaner(water_index="nir08", index_threshold=-1280.0)
     writer = CoastlineWriter(
