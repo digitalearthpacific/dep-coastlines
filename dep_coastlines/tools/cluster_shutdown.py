66f3490cdac07facbcf326c50f6fbb6e809c74c8
Jesse Anderson <jesse.anderson@gmail.com>
2024-05-28 15:35:39 -0700

    removed old MSPC code

--- dep_coastlines/tools/cluster_shutdown.py
+++
@@ -1,9 +0,0 @@
-from dask_gateway import Gateway
-
-g = Gateway(
-    address="https://pccompute.westeurope.cloudapp.azure.com/compute/services/dask-gateway",
-    auth="jupyterhub",
-)
-g.list_clusters()
-breakpoint()
-[g.connect(c.name).shutdown() for c in g.list_clusters()]
--- dep_coastlines/tools/run_on_pc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pip install "git+https://github.com/digitalearthpacific/dep-tools.git@develop"
-pip install "git+https://github.com/jessjaco/azure-logger.git"
-pip install "git+https://github.com/digitalearthpacific/dep-grid.git@cleanup"
-python "$@"
