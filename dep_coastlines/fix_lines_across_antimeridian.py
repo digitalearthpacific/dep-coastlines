74cc2b6a892979e56fbb489548fa058794c8cb1c
Jesse Anderson <jesse.anderson@gmail.com>
2024-06-12 08:50:11 -0700

    remove breakpoint

--- dep_coastlines/fix_lines_across_antimeridian.py
+++ dep_coastlines/fix_lines_across_antimeridian.py
@@ -7,7 +7,6 @@ from dep_tools.utils import shift_negative_longitudes
 def fix_lines_across_antimeridian(
     input_file: str, output_file: str, input_kwargs=dict(), output_kwargs=dict()
 ):
-    breakpoint()
     d = gpd.read_file(input_file, **input_kwargs).to_crs(4326)
     d["geometry"] = d.geometry.apply(shift_negative_longitudes)
     d.to_file(output_file, **output_kwargs)
