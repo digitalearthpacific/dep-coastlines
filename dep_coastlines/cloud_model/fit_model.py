61aa234c207f60ed50e6d74992e5af832cb6d4dd
Jesse Anderson <jesse.anderson@gmail.com>
2024-06-25 13:17:01 -0700

    model cleanup

---
+++ dep_coastlines/cloud_model/fit_model.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+from joblib import dump
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.base import BaseEstimator
+
+
+@dataclass
+class SavedModel:
+    model: BaseEstimator
+    training_data: pd.DataFrame
+    predictor_columns: list[str]
+    response_column: str
+    codes: pd.DataFrame
+
+
+def train(training_data):
+    codes = pd.DataFrame.from_records(
+        [
+            ("land", 1, 1, 0, 1, "#064a00"),
+            ("clean_water", 9, 2, 0, 2, "#ff0000"),
+            ("water", 2, 3, 0, 2, "#ff0000"),
+            ("deep_ocean", 3, 3, 0, 2, "#ff0000"),
+            ("noisy_water", 8, 7, 0, 2, "#00ffff"),
+            ("surf", 4, 4, 0, 4, "#ddfeff"),
+            ("coast", 5, 5, 0, 5, "#ffe48b"),
+            ("cloud", 6, 6, 1, 6, "#a9a9a9"),
+            ("noise", 7, 6, 1, 6, "#e16be1"),
+            ("bare_terrain", 10, 8, 0, 8, "#0000ff"),
+            ("built", 11, 9, 0, 9, "#ff0000"),
+        ],
+        columns=["code", "intcode", "simplintcode", "cloud", "unified_water", "color"],
+    )
+
+    removes = ["shift"]
+    training_data = pd.read_csv(training_data).query("code not in @removes")
+
+    training_data = training_data.join(codes.set_index("code"), on="code")
+    training_columns = [
+        "blue",
+        "blue_mad",
+        "count",
+        "green",
+        "green_mad",
+        "nir08",
+        "nir08_mad",
+        "red",
+        "red_mad",
+        "swir16",
+        "swir16_mad",
+        "swir22",
+        "swir22_mad",
+        "twndwi",
+        "twndwi_mad",
+        "twndwi_stdev",
+    ]
+    training_data = training_data.dropna(subset=training_columns)
+    X = training_data.loc[:, training_columns].to_numpy()
+    response_column = "cloud"
+    y = training_data[response_column].to_numpy()
+
+    m = RandomForestClassifier()
+    full_model = m.fit(X, y)
+    return SavedModel(
+        model=full_model,
+        training_data=training_data,
+        predictor_columns=training_columns,
+        response_column=response_column,
+        codes=codes,
+    )
+
+
+if __name__ == "__main__":
+    training_data_file = "../data/training_data_with_features_0-7-0-4_17May2024.csv"
+    output = train(training_data_file)
+    dump(output, "../dep_coastlines/full_model_17May2024.joblib")
--- dep_coastlines/cloud_model/prep_training_data.py
+++ dep_coastlines/cloud_model/prep_training_data.py
@@ -1,12 +1,23 @@
 from dataclasses import dataclass
 import geopandas as gpd
 import pandas as pd
+from sklearn.base import BaseEstimator
 from xarray import concat
 
 from dea_tools.classification import predict_xr
 from dep_tools.namers import DepItemPath
 
 from dep_coastlines.MosaicLoader import MosaicLoader
+
+
+def prep_training_data(input_file, output_file):
+    load_point_values(gpd.read_file(input_file)).to_csv(output_file, index=False)
+
+
+def load_point_values(points):
+    return (
+        split_multiyears(cast_all(points)).groupby("time").apply(pull_data_for_datetime)
+    )
 
 
 def split_multiyears(df, time_column="time", splitter=","):
@@ -22,6 +33,25 @@ def cast_all(df, time_column="time"):
     # for now just make it the biggest mosaic
     df.loc[df[time_column] == "all", time_column] = "2020/2022"
     return df
+
+
+def pull_data_for_datetime(df):
+    itempath = DepItemPath(
+        sensor="ls",
+        dataset_id="coastlines/mosaics-corrected",
+        version="0.7.0.4",
+        time=df.time.iloc[0].replace("/", "_"),
+        zero_pad_numbers=True,
+    )
+    loader = MosaicLoader(itempath=itempath, add_deviations=False)
+
+    def _pull_data_for_cell(group):
+        ds = loader.load(group.set_index(["row", "column"]))
+        print(f"{df.time.iloc[0]}|{group.row.iloc[0]}|{group.column.iloc[0]}")
+        return add_image_values(group, ds)
+
+    output = df.groupby(["row", "column"]).apply(_pull_data_for_cell)
+    return output
 
 
 def add_image_values(pts: gpd.GeoDataFrame, image) -> gpd.GeoDataFrame:
@@ -44,48 +74,13 @@ def add_image_values(pts: gpd.GeoDataFrame, image) -> gpd.GeoDataFrame:
     return output
 
 
-def pull_data_for_datetime(df):
-    itempath = DepItemPath(
-        sensor="ls",
-        dataset_id="coastlines/mosaics-corrected",
-        version="0.7.0.4",
-        time=df.time.iloc[0].replace("/", "_"),
-        zero_pad_numbers=True,
-    )
-    loader = MosaicLoader(itempath=itempath, add_deviations=False)
-
-    def _pull_data_for_cell(group):
-        ds = loader.load(group.set_index(["row", "column"]))
-        print(f"{df.time.iloc[0]}|{group.row.iloc[0]}|{group.column.iloc[0]}")
-        return add_image_values(group, ds)
-
-    output = df.groupby(["row", "column"]).apply(_pull_data_for_cell)
-    return output
-
-
-def load_point_values(points):
-    return (
-        split_multiyears(cast_all(points)).groupby("time").apply(pull_data_for_datetime)
-    )
-
-
-def prep_training_data():
-    load_point_values(gpd.read_file("data/training_data_v7.gpkg")).to_csv(
-        "data/training_data_with_features_0-7-0-4_13May2024.csv", index=False
-    )
-
-
-from sklearn.base import BaseEstimator
-from pandas import DataFrame
-
-
 @dataclass
 class SavedModel:
     model: BaseEstimator
-    training_data: DataFrame
+    training_data: pd.DataFrame
     predictor_columns: list[str]
     response_column: str
-    codes: DataFrame
+    codes: pd.DataFrame
 
 
 class ModelPredictor:
@@ -136,4 +131,6 @@ class ModelPredictor:
 
 
 if __name__ == "__main__":
-    prep_training_data()
+    input_file = "data/training_data_v7.gpkg"
+    output_file = "data/training_data_with_features_0-7-0-4_13May2024.csv"
+    prep_training_data(input_file, output_file)
