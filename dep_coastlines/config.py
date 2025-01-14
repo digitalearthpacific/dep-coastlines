7c5395a73354a0f33a27d2ccd54daa3964d2b0fe
Jesse Anderson <jesse.anderson@gmail.com>
2025-01-14 13:26:35 -0700

    bump mosaic version

--- dep_coastlines/config.py
+++ dep_coastlines/config.py
@@ -5,7 +5,7 @@ HTTPS_PREFIX = f"https://{BUCKET}.s3.{_region}.amazonaws.com"
 AOI_URL = f"{HTTPS_PREFIX}/aoi/aoi.gpkg"
 CURRENT_COASTLINES_OUTPUT = "data/processed/0-7-0-54/dep_ls_coastlines_0-7-0-54.gpkg"
 
-MOSAIC_VERSION = "0.8.0"
+MOSAIC_VERSION = "0.8.1"
 MOSAIC_DATASET_ID = "coastlines/interim/mosaic"
 
 CLOUD_MODEL_VERSION = "0.8.0"
@@ -13,6 +13,6 @@ CLOUD_MODEL_FILE = (
     f"dep_coastlines/cloud_model/cloud_model_{CLOUD_MODEL_VERSION}.joblib"
 )
 
-VECTOR_VERSION = "0-8-0"
+VECTOR_VERSION = "0-8-1"
 VECTOR_DATASET_ID = "coastlines/interim/coastlines"
 VECTOR_DATETIME = "1998/2023"
